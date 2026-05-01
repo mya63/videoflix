171830fb76f7867d1043cb2dc77fa81fae041332
mya63 <muhammedyunusamini4@gmail.com>
2026-05-01 22:34:15 +0200

    Add SMTP email configuration

--- core/settings.py
+++ core/settings.py
@@ -166,3 +166,13 @@ REST_FRAMEWORK = {
         'authentication.authentication.CookieJWTAuthentication',
     ],
 }
+
+# EMAIL CONFIG
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
