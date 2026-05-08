557bd5d51ef4cbb1fb8b7be00509da40e429d315
mya63 <muhammedyunusamini4@gmail.com>
2026-05-08 17:31:05 +0200

    fix cookie auth core

--- core/settings.py
+++ core/settings.py
@@ -169,6 +169,7 @@ CSRF_COOKIE_SECURE = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
         'authentication.authentication.CookieJWTAuthentication',
     ],
 }
